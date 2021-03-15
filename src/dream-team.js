1cd47688631cfda49748fd221fa48caaa61d5830
geykeranna <62444758+geykeranna@users.noreply.github.com>
2021-03-15 14:50:05 +0300

    feat: add function body

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,8 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members))
+  return false
+  let buff = members.filter(i => typeof i == 'string').map(item => item.toUpperCase().split('').filter(letter => letter !== ' ').join('')[0]).sort().join('');// фильтруем строку
+  return buff;
 };
