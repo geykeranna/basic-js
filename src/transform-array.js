ec729fc32081a6520dcd2aaa13a2a00fb8e9689c
geykeranna <62444758+geykeranna@users.noreply.github.com>
2021-03-15 20:21:25 +0300

    feat: add function body

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) {
+    throw new Error();
+  }
+  let array = [];
+  for(let i = 0; i < arr.length; i++){
+
+    if(arr[i] === "--discard-next") {
+        i++;
+    }
+    else if (arr[i] === "--discard-prev") {
+        if (!(arr[i-2] === '--discard-next' && i !== 0)){
+          array.pop();
+        }
+    }
+    else if (arr[i] === "--double-next") {
+        if(i !== arr.length - 1)
+          array.push(arr[i+1]);
+    }
+    else if (arr[i] === "--double-prev") {
+      if (!(i === 0 ||  arr[i-2] === '--discard-next'))
+        array.push(arr[i - 1]);
+    } else {
+      array.push(arr[i]);
+    }
+  }
+  return array;
 };
